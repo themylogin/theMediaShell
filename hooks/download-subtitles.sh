1516fe351dbdf9e7a5e0245dd7f6d17b1067f61e
themylogin <themylogin@gmail.com>
2015-10-25 13:31:30 +0600

    fix hooks/download-subtitles.sh

--- hooks/download-subtitles.sh
+++ hooks/download-subtitles.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-IFS="\n"
-for i in `find -type f -regex ".*/.*\.\(avi\|mkv\|mp4\)"`;
-do
-    subliminal download -l en "$i"
-done
+find -type f -regex ".*/.*\.\(avi\|mkv\|mp4\)" | sort | xargs subliminal download -l en "$i"
